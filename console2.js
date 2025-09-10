e75a95fe0173db4cc7a62d1f981ce5b728a388d3
P200p <taislot1999@gmail.com>
2025-09-10 11:23:58 +0700

    Update console2.js

--- console2.js
+++ console2.js
@@ -108,10 +108,7 @@
     <div class="gc2-body">
       <div class="gc2-label">📋 โค้ด (Javascript)</div><textarea class="gc2-text" id="gc2Code" placeholder="javascript:alert('Hello from Goonee!')"></textarea>
       <div class="gc2-label">🧾 ผลลัพธ์</div><pre class="gc2-output" id="gc2Out"></pre>
-    </div> <div class="gc2-resizer tl"></div>
-<div class="gc2-resizer tr"></div>
-<div class="gc2-resizer bl"></div>
-<div class="gc2-resizer br"></div>
+    </div> 
 
     <div class="gc2-footer"><div id="gc2Status">พร้อมใช้งาน</div><div id="gc2ThemeName" class="gc2-badge">Matrix</div></div>
   `;
